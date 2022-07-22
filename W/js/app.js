25040604a31512ac87a2ad7d0cf71c97d2387679
Zach <zacbenincasa@gamil.com>
2022-07-21 19:53:11 -0600

    sorry for taking so long

---
+++ W/js/app.js
@@ -0,0 +1,22 @@
+function math_time(number1,number2){
+//this is a trial version of w10-a.1
+    number1+number2
+    return number1+number2
+}
+
+
+function split(string){
+    if(string>10){
+        console.log(true)
+    
+    }else{
+        console.log(false)
+    }
+}
+
+function theory(array_of_strings){
+    let counter=0
+    while (counter<array_of_strings.length){
+        console.log(array_of_strings.starts_with,"ph")
+    }
+}
