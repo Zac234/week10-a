0b21a80f37a145aa339217bae7ab6bd15784e34a
Zach <zacbenincasa@gamil.com>
2022-07-21 19:59:48 -0600

    sorry this is late

--- W/js/app.js
+++ W/js/app.js
@@ -1,19 +1,22 @@
 function math_time(number1,number2){
 //this is a trial version of w10-a.1
-    number1+number2
-    return number1+number2
+    let number1=1
+    let number2=2
+    let answer=number1+number2
+    return answer     
+
 }
 
-
+// finished version of w10-a.2
 function split(string){
-    if(string>10){
+    if(string<10){
         console.log(true)
     
     }else{
         console.log(false)
     }
 }
-
+//finished version of w10-a.3
 function theory(array_of_strings){
     let counter=0
     while (counter<array_of_strings.length){
